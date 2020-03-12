ccd090a74f87930216e74a44a92b0426d0e509b4
Kiefer Ragay <kiefer.m.ragay@hotmail.com>
2020-03-11 18:12:37 -0700

    Attempt to Emulate document.getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,59 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+var collection = [];
+  
+  //scan through HTML document,
+  
+
+  //how do you specify function to scan through HTML coding in another file?
+
+  //You should use document.body, element.childNodes, and element.classList
+  //stores the body of the HTML document, but only initially
+  
+
+  if(this === document) {
+  var mainBody = document.body;
+  var nodes = mainBody.childNodes;
+  console.log(mainBody);
+  console.log(nodes);
+  } 
+  else {
+  	var nodes = this.childNodes;
+  	console.log(nodes);
+  }
+
+  
+
+  //create an object to hold the element information for the HTML collection
+  
+  
+  //be sure to use recursion
+
+  /*if an HTML string contains 'class = "className"' 
+  then scan through it to collect required information for the object*/
+  //string in this case is the child element within the html body
+  //see if class list contains the class argument that's input into the function
+for(var i = 0; i < nodes.length; i++) {
+	  if(_.contains(nodes[i].classList, className)) {
+    collection.push(nodes[i]);
+
+    //does not account for subordinate child nodes
+    //need to iterate through lower levels and push them into the collection as well
+    //nodes[i] may not necessarily be the correct object to push in...but what exactly should be pushed into the htmlCollection array?
+    //push that object into an array
+      };
+     if(nodes[i].childNodes.length) {
+		collection.push(nodes[i].getElementsByClassName(className));
+	 }
+	}
+
+
+  
+  //return the array
+  return collection;
+  
 };
+
+document.getElementsByClassName("targetClassName");
+console.log(JSON.stringify(document.body.childNodes));
